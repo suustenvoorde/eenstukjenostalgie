8e4dd2e0be52db4efeb62ff9cbed87bf41523fd3
Max de Vries <max.devries.1995@gmail.com>
2020-05-11 14:24:48 +0200

    Change name new-story-submit and update the submit

---
+++ public/js/components/submitLocationTimestamp.js
@@ -0,0 +1,32 @@
+var map = require('./map.js');
+
+module.exports = {
+  form: document.querySelector('.location-timestamp'),
+  init: function () {
+    this.form.addEventListener('submit', (e) => {
+      var formdata = new FormData(e.target);
+      var data = {
+        wkt: map.inputCircle.wkt,
+        coords: map.inputCircle.coords
+      };
+
+      for (var key of formdata.keys()) {
+        data[key] = formdata.get(key);
+      }
+
+      var http = new XMLHttpRequest();
+      var url = '/create-story';
+
+      http.open('post', url, true);
+      http.setRequestHeader('Content-type', 'application/json');
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          console.log(http.responseURL);
+          window.location = http.responseURL;
+        }
+      }
+      http.send(JSON.stringify(data));
+      e.preventDefault();
+    });
+  }
+};
