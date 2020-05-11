bd8ff8237f4bf810af7980c55c82fd205d3b5e4e
Max de Vries <max.devries.1995@gmail.com>
2020-05-11 14:50:50 +0200

    Restore functions to get images from current position on the map and timestamp

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -3,9 +3,6 @@ var circleToPolygon = require('./circletopolygon.js');
 var toWKT = require('./towkt.js');
 var search = require('./search.js');
 
-// Set global wkt variable:
-var inputCircle;
-
 module.exports = {
 	mapElem: document.getElementById('map'),
 	searchbar: document.querySelector('.searchbar'),
@@ -15,10 +12,10 @@ module.exports = {
 		zoomControl: false
 	}),
 	circle: L.circle(),
-	// polygon: L.polygon({
-	// 	color: '#DA121A'
-	// }),
-	// geoJSON: L.geoJSON(),
+	polygon: L.polygon({
+		color: '#0000FF'
+	}),
+	geoJSON: L.geoJSON(),
 	centerLatLng: [ 52.370216, 4.895168 ],
 	startPos: { x: 0, y: 0 },
 	currentPos: { x: 0, y: 0 },
@@ -50,6 +47,13 @@ module.exports = {
 			.setRadius(this.radiusSelect.value / 2)
 			.addTo(this.map);
 
+		// Make the circle draggable:
+		var draggableCircle = new L.Draggable(this.circle._path);
+		draggableCircle.enable();
+
+		// Create the polygon, with the centerLatLng as coords:
+		this.createPolygon(this.centerLatLng);
+
 		// Add start and current position:
 		this.startPos.x = this.circle._point.x;
 		this.startPos.y = this.circle._point.y;
@@ -57,16 +61,26 @@ module.exports = {
 
 		var originCenterPoint = this.map.latLngToLayerPoint(this.centerLatLng);
 
+		// Get all the streets:
+		var allStreets = await this.getAllStreets();
+
+		// Map the street names from allStreets for search:
+		var streetNames = allStreets.map(street => street.properties.name);
+
+		// Initialize the autocomplete search:
+		search.init(this.searchbar, streetNames);
+
+		// Add the streets data to geoJSON:
+		this.geoJSON.addData(allStreets);
+
 		// On map drag:
 		this.map.on('drag', (e) => {
-			// console.log(this.mapSize);
-			// console.log(e.sourceTarget._newPos);
 			this.currentPos.x = this.startPos.x + e.sourceTarget._newPos.x - this.distance.x - this.mapSize.x;
 			this.currentPos.y = this.startPos.y + e.sourceTarget._newPos.y - this.distance.y - this.mapSize.y;
 			this.moveRadiusSelect(this.currentPos);
 		});
 
-		// On dragend:
+		// On map dragend:
 		this.map.on('dragend', (e) => {
 			this.startPos.x = this.currentPos.x;
 			this.startPos.y = this.currentPos.y;
@@ -75,7 +89,7 @@ module.exports = {
 			this.distance.y = e.sourceTarget._newPos.y;
 		});
 
-		// On zoomend:
+		// On map zoomend:
 		this.map.on('zoomend', (e) => {
 			var newZoomLevel = Number(e.sourceTarget._animateToZoom);
 			var newCenterPoint = this.map.latLngToLayerPoint(this.centerLatLng);
@@ -89,9 +103,11 @@ module.exports = {
 			this.distance.y = originCenterPoint.y - newCenterPoint.y;
 
 			this.moveRadiusSelect(this.startPos);
+			var latLng = this.map.layerPointToLatLng(this.currentPos);
+			this.createPolygon([latLng.lat, latLng.lng]);
 		});
 
-		// On resize:
+		// On map resize:
 		this.map.on('resize', (e) => {
 			var size = {
 				x: (e.newSize.x - e.oldSize.x) / 2,
@@ -107,80 +123,27 @@ module.exports = {
 			this.moveRadiusSelect(this.startPos);
 		});
 
-		// Event when you move the circle:
+		// This still has to work:
 
-		// Initialize circle events:
-		// this.changeRadius();
-
-		// Create the polygon, with the centerPoint as coords:
-		// this.createPolygon(this.centerPoint);
-
-		// Get all the streets:
-		// var allStreets = await this.getAllStreets();
-
-		// Map the street names from allStreets for search:
-		// var streetNames = allStreets.map(street => street.properties.name);
-
-		// Initialize the autocomplete search:
-		// search.init(this.searchbar, streetNames);
-
-		// Add the streets data to geoJSON:
-		// this.geoJSON.addData(allStreets);
-
-		// Dragging the circle:
-		// var draggable = new L.Draggable(this.circle._path);
-		// draggable.enable();
-		//
-		// this.startPos.x = this.circle._point.x;
-		// this.startPos.y = this.circle._point.y;
-		//
-		// // Calculate the new center:
-		// draggable.on('drag', (e) => {
-		// 	this.currentPos.x = e.sourceTarget._newPos.x;
-		// 	this.currentPos.y = e.sourceTarget._newPos.y;
-		// 	this.moveCircle();
-		// });
-		//
-		// this.map.on('zoom', (e) => {
-		// 	var newZoomLevel = Number(e.sourceTarget._animateToZoom);
-		// 	var layerPoint = this.map.latLngToLayerPoint(this.centerPoint);
-		// 	this.map.setView(this.centerPoint, newZoomLevel);
-		// 	this.startPos.x = layerPoint.x - this.currentPos.x;
-		// 	this.startPos.y = layerPoint.y - this.currentPos.y;
-		// 	this.moveCircle();
-		// });
-	},
-	getAllStreets: async function () {
-		return fetch('/js/streets.json')
-			.then(res => res.json())
-			.catch(err => {
-				console.log(err);
-			});
-	},
-	changeRadius: function () {
-		this.selectRadius.addEventListener('change', (e) => {
-			var latlng = this.circle.getLatLng();
-			var meters = (e.target.value / 2) * 1000;
-			this.createCircle(Object.values(latlng), meters);
-			this.createPolygon(this.centerPoint, meters);
+		// On circle drag:
+		draggableCircle.on('drag', (e) => {
+			this.currentPos.x = this.startPos.x + e.sourceTarget._newPos.x - this.distance.x - this.mapSize.x;
+			this.currentPos.y = this.startPos.y + e.sourceTarget._newPos.y - this.distance.y - this.mapSize.y;
+			this.moveRadiusSelect(this.currentPos);
 		});
-	},
-	moveCircle: function () {
-		var x = this.startPos.x + this.currentPos.x;
-		var y = this.startPos.y + this.currentPos.y;
-		var point = { x: x, y: y };
-		var latlng = this.map.layerPointToLatLng(point);
-		var radius = this.circle.getRadius();
 
-		// Create the new polygon:
-		this.centerPoint = Object.values(latlng);
-		this.createCircle(Object.values(latlng), radius);
-		this.createPolygon(Object.values(latlng), radius);
-		L.DomUtil.setTransform(this.circle._path, { x: 0, y: 0 });
-	},
-	createCircle: function (coords, radius = this.circle.getRadius()) {
-		this.circle.setLatLng(coords);
-		this.circle.setRadius(radius);
+		// On circle dragend:
+		draggableCircle.on('dragend', (e) => {
+			this.startPos.x = this.currentPos.x;
+			this.startPos.y = this.currentPos.y;
+
+			this.distance.x = e.sourceTarget._newPos.x;
+			this.distance.y = e.sourceTarget._newPos.y;
+
+			// Move polygon to circle's location:
+			var latLng = this.map.layerPointToLatLng(this.currentPos);
+			this.createPolygon([latLng.lat, latLng.lng]);
+		});
 	},
 	createPolygon: function (coords, radius = this.circle.getRadius(), numberOfEdges = 10) {
 		//leaflet polygon to wkt
@@ -188,35 +151,41 @@ module.exports = {
 
 		// Set the new coords:
 		this.polygon
-			.setLatLngs(polygonCoords.coordinates[0]);
+			.setLatLngs(polygonCoords.coordinates[0])
+			// .addTo(this.map) // Remove for production
+			// .bringToBack(); // Remove for production
 
 		// Create a wkt from the polygon:
-		inputCircle = {
+		this.inputCircle = {
 			wkt: toWKT(this.polygon),
 			coords: coords
 		};
+	},
+	getAllStreets: async function () {
+		return fetch('/js/streets.json')
+			.then(res => res.json())
+			.catch(err => {
+				console.log(err);
+			});
 	}
 };
 
-exports.selectedStreet = function (streetName) {
-	map.geoJSON.eachLayer(layer => {
-		if (layer.feature.properties.name === streetName) {
-			var bounds = layer.getBounds();
-			var center = bounds.getCenter();
-
-			var layerPoint = map.map.latLngToLayerPoint([center.lat, center.lng]);
-			map.map.setView([center.lat, center.lng], 14);
-
-			map.startPos.x = layerPoint.x - map.currentPos.x;
-			map.startPos.y = layerPoint.y - map.currentPos.y;
-
-			map.moveCircle();
-			map.createCircle([center.lat, center.lng]);
-			map.createPolygon([center.lat, center.lng]);
-		}
-	});
-}
-
-exports.inputCircle = function () {
-	return inputCircle;
-}
+// Find a way to make this work:
+// exports.selectedStreet = function (streetName) {
+// 	map.geoJSON.eachLayer(layer => {
+// 		if (layer.feature.properties.name === streetName) {
+// 			var bounds = layer.getBounds();
+// 			var center = bounds.getCenter();
+//
+// 			var layerPoint = map.map.latLngToLayerPoint([center.lat, center.lng]);
+// 			map.map.setView([center.lat, center.lng], 14);
+//
+// 			map.startPos.x = layerPoint.x - map.currentPos.x;
+// 			map.startPos.y = layerPoint.y - map.currentPos.y;
+//
+// 			map.moveCircle();
+// 			map.createCircle([center.lat, center.lng]);
+// 			map.createPolygon([center.lat, center.lng]);
+// 		}
+// 	});
+// }
