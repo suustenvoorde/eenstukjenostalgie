36d8264155f65962bed739e7b81e781260be49c8
Max de Vries <max.devries.1995@gmail.com>
2020-05-11 17:18:06 +0200

    Fixed issues with map and searchbar

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-const map = require('./components/map.js');
-const instructionSlides = require('./components/instructionSlides.js');
-const submitLocationTimestamp = require('./components/submitLocationTimestamp.js');
-
-(function () {
-  map.init();
-  instructionSlides.init();
-  submitLocationTimestamp.init();
-}) ();
