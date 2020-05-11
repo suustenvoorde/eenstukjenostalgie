2ff3b205654bddcdbfb0ff0305464f63f722725f
Max de Vries <max.devries.1995@gmail.com>
2020-05-11 14:25:20 +0200

    Add component submitLocationTimestamp.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 const map = require('./components/map.js');
 const instructionSlides = require('./components/instructionSlides.js');
+const submitLocationTimestamp = require('./components/submitLocationTimestamp.js');
 
 (function () {
   map.init();
   instructionSlides.init();
+  submitLocationTimestamp.init();
 }) ();
