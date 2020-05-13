be62afd62e5dd91407061383bfe2907064122ac9
Max de Vries <max.devries.1995@gmail.com>
2020-05-13 19:26:06 +0200

    Remove my-story and save-story routes as well as other unused routes

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,4 @@ router.post('/create-story', controller.postCreateStoryPage);
 // GET create story page:
 router.get('/create-story/:id', controller.getCreateStoryPage);
 
-// POST my story page:
-router.post('/my-story/:id', controller.postMyStoryPage);
-
-// GET story page:
-router.get('/my-story/:id', controller.getMyStoryPage);
-
-// GET save story page:
-router.get('/save-story/:id', controller.saveStoryPage);
-
-// GET edit story page:
-router.get('/edit-story/:id', controller.editStoryPage);
-
-// GET photobook page;
-router.get('/photobook', controller.photoBookPage);
-
 module.exports = router;
