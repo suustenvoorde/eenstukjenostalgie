1f43c69471d090ae735431c7840562fb3dadf28d
Max de Vries <max.devries.1995@gmail.com>
2020-05-14 15:25:07 +0200

    Create database controller

---
+++ controllers/database.js
@@ -0,0 +1,8 @@
+var MongoClient = require('mongodb').MongoClient;
+var client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+
+const database = {
+
+};
+
+module.exports = database;
