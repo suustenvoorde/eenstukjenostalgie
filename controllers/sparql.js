7dd819e8d98e2b48d85e58a26cd977af2592099c
Max de Vries <max.devries.1995@gmail.com>
2020-05-15 14:38:28 +0200

    Remove getStreetWkts because we don't need it anymore

--- controllers/sparql.js
+++ controllers/sparql.js
@@ -21,33 +21,9 @@ var sparqlqueries = {
     `;
     return this.url(query);
   },
-  getStreetWkts: function (wkt) {
-    const query = `
-      PREFIX dct: <http://purl.org/dc/terms/>
-      PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-      PREFIX void: <http://rdfs.org/ns/void#>
-      PREFIX hg: <http://rdf.histograph.io/>
-      PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-      PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
-      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-      PREFIX dc: <http://purl.org/dc/elements/1.1/>
-      SELECT ?uri ?label ?wkt WHERE {
-        ?uri a hg:Street ;
-        geo:hasGeometry/geo:asWKT ?wkt ;
-        rdfs:label ?label .
-
-        BIND (bif:st_geomfromtext("${wkt}") as ?circle)
-        BIND (bif:st_geomfromtext(?wkt) AS ?streetGeo)
-        FILTER(bif:GeometryType(?streetGeo)!='POLYGON' && bif:st_intersects(?streetGeo, ?circle))
-      }
-    `;
-    return this.url(query);
-  },
-  getLocationAndTimestamp: function (data) {
-    const beginTimestamp = `${data.startyear}-01-01`;
-    const endTimestamp = `${data.endyear}-12-31`;
-    const wkt = data.wkt
+  getLocationAndTimestamp: function (startyear, endyear, wkt) {
+    const beginTimestamp = `${startyear}-01-01`;
+    const endTimestamp = `${endyear}-12-31`;
 
     const query = `
       PREFIX dct: <http://purl.org/dc/terms/>
@@ -59,7 +35,7 @@ var sparqlqueries = {
       PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
       PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
       PREFIX dc: <http://purl.org/dc/elements/1.1/>
-      SELECT ?title ?img ?start ?end ?street ?streetLabel WHERE {
+      SELECT ?title ?img ?start ?end ?street ?streetLabel ?wkt WHERE {
         # basic data
         ?cho dc:title ?title .
         ?cho foaf:depiction ?img .
@@ -76,10 +52,10 @@ var sparqlqueries = {
         # spatial filter
         ?cho dct:spatial ?street .
         ?street a hg:Street ;
-        geo:hasGeometry/geo:asWKT ?streetWkt ;
+        geo:hasGeometry/geo:asWKT ?wkt ;
         rdfs:label ?streetLabel .
         BIND (bif:st_geomfromtext("${wkt}") as ?x)
-        BIND (bif:st_geomfromtext(?streetWkt) AS ?y)
+        BIND (bif:st_geomfromtext(?wkt) AS ?y)
         FILTER(bif:GeometryType(?y)!='POLYGON' && bif:st_intersects(?x, ?y))
       }
       ORDER BY ?start
