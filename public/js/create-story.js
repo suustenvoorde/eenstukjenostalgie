67fd8581416afd0e622c90d4ecf1b2c0981ffdad
Max de Vries <max.devries.1995@gmail.com>
2020-05-18 23:14:16 +0200

    Add shareModal

--- public/js/create-story.js
+++ public/js/create-story.js
@@ -1,10 +1,12 @@
 var imageDetail = require('./components/image-detail.js');
 var share = require('./components/share.js');
+var shareModal = require('./components/share-modal.js');
 
 const createStory = {
   init: function () {
     imageDetail.init();
     share.init();
+    shareModal.init();
   }
 };
 
