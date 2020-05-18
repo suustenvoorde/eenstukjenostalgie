2d446e6b366b8154f9147596dc5f0b8e7a78ae21
Max de Vries <max.devries.1995@gmail.com>
2020-05-18 15:44:37 +0200

    Add main and create-story js files with embedded components

--- public/js/create-story.js
+++ public/js/create-story.js
@@ -1,1 +1,11 @@
+var imageDetail = require('./components/image-detail.js');
+var share = require('./components/share.js');
 
+const createStory = {
+  init: function () {
+    imageDetail.init();
+    share.init();
+  }
+};
+
+createStory.init();
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+var map = require('./components/map.js');
+var instructionSlides = require('./components/instructionSlides.js');
+var submitLocationTimestamp = require('./components/submitLocationTimestamp.js');
 
+const main = {
+  init: function () {
+    map.init();
+    instructionSlides.init();
+    submitLocationTimestamp.init();
+  }
+};
+
+main.init();
