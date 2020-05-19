4f0f9657d6412dc116714e2e188fa1a14a41c23b
Max de Vries <max.devries.1995@gmail.com>
2020-05-19 23:41:30 +0200

    Fetch the photo selection for new photos

--- public/js/components/lazy-load.js
+++ public/js/components/lazy-load.js
@@ -1,6 +1,38 @@
 const lazyLoad = {
-  init: function () {
-    console.log('works');
+  images: document.querySelectorAll('.lazy'),
+  fetchPhotoSelection: async function (startIdx) {
+    var pathname = window.location.pathname.split('/');
+    var id = pathname[pathname.length-1];
+
+    // Fetch new photos:
+    return await fetch ('/photo-selection/' + id + '/' + startIdx)
+      .then(res => res.json())
+      .catch(err => console.log(err));
+  },
+  init: async function () {
+    // ...Create img element and place in correct year:
+    // Maybe for extra beauty: Add placeholders for photos to come:
+
+    var scrolling = true;
+    var finalImg = this.images[this.images.length-1];
+    var finalImgTop, scrollTop;
+
+    // Calc the offsetTop of the final image:
+    window.addEventListener('load', (e) => {
+      finalImgTop = finalImg.offsetTop;
+    });
+
+    // Determine if scroll position >= position final img:
+    document.addEventListener('scroll', async (e) => {
+      if (scrolling) {
+        scrollTop = window.scrollY;
+        if (scrollTop >= finalImgTop) {
+          scrolling = false;
+          var selection = await this.fetchPhotoSelection(this.images.length);
+          console.log(selection);
+        }
+      }
+    });
   }
 };
 
