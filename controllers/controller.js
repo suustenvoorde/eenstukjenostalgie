4170f9a44856561e1a4036e2a54bb8cb239c097b
Max de Vries <max.devries.1995@gmail.com>
2020-05-25 16:21:43 +0200

    Add shared photo url to create story page

--- controllers/controller.js
+++ controllers/controller.js
@@ -50,6 +50,7 @@ exports.useCreateStoryPage = async function (req, res, next) {
     await database.getItem(database.photos, req.query['shared'])
       .then(result => {
         req.sharedPhoto = {
+          url: req.hostname + '/photo/' + result.id,
           src: result.src,
           alt: result.alt
         };
