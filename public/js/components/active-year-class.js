484521e128acd4e157a269e0dea6c33b291c079a
solideagency <suus.ten.voorde@hva.nl>
2020-05-25 11:38:23 +0200

    already the first active-year

--- public/js/components/active-year-class.js
+++ public/js/components/active-year-class.js
@@ -3,11 +3,17 @@ const hamburgerMenu = {
     var sections = document.querySelectorAll('section.year');
     var main = document.querySelector('body');
     var yearTitle = document.querySelectorAll('.year-title');
+    yearTitle[0].classList.add('active-year');
+
     window.addEventListener('scroll', function(){
       for (var i = 0; i < sections.length; i++) {
         if(sections[i].offsetTop <= window.scrollY && (sections[i].offsetTop + sections[i].offsetHeight) > window.scrollY+5) {
           yearTitle[i].classList.add('active-year');
-        } else {
+        } else if (window.scrollY < 67) {
+          console.log(window.scrollY, sections[i].offsetTop)
+          yearTitle[0].classList.add('active-year');
+        }
+        else {
           yearTitle[i].classList.remove('active-year');
         }
       }
