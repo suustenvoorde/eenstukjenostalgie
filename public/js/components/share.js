ce5ef22e58d2ad6856d593cddb1c4b193311ea4f
Max de Vries <max.devries.1995@gmail.com>
2020-05-26 17:05:22 +0200

    Add share button and its functionality to the lazy load

--- public/js/components/share.js
+++ public/js/components/share.js
@@ -3,33 +3,36 @@ var shareModal = require('./share-modal.js');
 
 const share = {
   elems: document.querySelectorAll('.share-btn'),
-  init: async function () {
+  postPhoto: async function (shareBtn) {
+    var img = shareBtn.parentNode.querySelector('img');
+    var photo = {
+      src: img.src,
+      alt: img.alt
+    };
+
+    fetch ('/photo', {
+      method: 'post',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(photo)
+    })
+    .then(res => res.json())
+    .then(photo => {
+      if (imageDetail.modal.classList.contains('show')) imageDetail.modal.classList.remove('show');
+      shareModal.openModal(photo.id);
+    })
+    .catch(err => console.log(err));
+  },
+  init: function () {
     this.elems.forEach(elem => {
       elem.addEventListener('click', (e) => {
-        var img = elem.parentNode.querySelector('img');
-        var photo = {
-          src: img.src,
-          alt: img.alt
-        };
-
-        fetch ('/photo', {
-          method: 'post',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          redirect: 'follow',
-          referrerPolicy: 'no-referrer',
-          body: JSON.stringify(photo)
-        })
-        .then(res => res.json())
-        .then(photo => {
-          if (imageDetail.modal.classList.contains('show')) imageDetail.modal.classList.remove('show');
-          shareModal.openModal(photo.id);
-        })
-        .catch(err => console.log(err));
+        this.postPhoto(elem);
         e.preventDefault();
       });
     });
