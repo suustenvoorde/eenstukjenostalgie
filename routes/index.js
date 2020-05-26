05327e53ed9d0b168bf314dd6cf177e591910e18
Max de Vries <max.devries.1995@gmail.com>
2020-05-26 20:18:11 +0200

    Change route photo to foto

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ var controller = require('../controllers/controller.js');
 // GET new story page:
 router.get('/', controller.homepage);
 
+// POST photo share page:
+router.post('/foto', controller.postPhotoPage);
+
+// GET photo share page:
+router.get('/foto/:id', controller.getPhotoPage);
+
 // POST create story page:
 router.post('/create-story', controller.postCreateStoryPage);
 
@@ -13,11 +19,5 @@ router.get('/:street/:id', controller.getCreateStoryPage);
 
 // GET photo selection page:
 router.get('/photo-selection/:id/:startIdx', controller.getPhotoSelectionPage);
-
-// POST photo share page:
-router.post('/photo', controller.postPhotoPage);
-
-// GET photo share page:
-router.get('/photo/:id', controller.getPhotoPage);
 
 module.exports = router;
