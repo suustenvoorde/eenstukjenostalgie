2069266c1ab7f962aeba7e20d66bba5f42fbf541
Max de Vries <max.devries.1995@gmail.com>
2020-05-28 13:11:44 +0200

    Add basic client side error handling

--- public/js/components/errors.js
+++ public/js/components/errors.js
@@ -1,5 +1,20 @@
+const status = {
+  incorrectTimestamp: {
+    msg: "Incorrect ingevulde jaartallen."
+  },
+  noSelectionFound: {
+    msg: "Kan geen nieuwe foto's laden."
+  },
+  noPhotoToDB: {
+    msg: "Kan de foto niet opslaan in de database."
+  }
+};
+
 const errors = {
-  
+  fire: function (name) {
+    var msg = status[name].msg;
+    console.log(msg);
+  }
 };
 
 module.exports = errors;
